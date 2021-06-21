1d0ebdc5705e86142c822238cabd2e64f7592e82
Philip Blenk <P.blenk@hotmail.com>
2021-06-21 11:06:00 +0200

    addedOneMoreArray

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-        int[] myIntArray = new int[5];
+        int[] myIntArray = new int[6];
         myIntArray[0] = 15;
         myIntArray[1] = 5;
         myIntArray[2] = 13;
         myIntArray[3] = 99;
         myIntArray[4] = 1;
-
-
-
+        myIntArray[5] = 193;
     }
 }
