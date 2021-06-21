a51f8369102e91ef07ef17e74ea055ded99a8e6b
Philip Blenk <P.blenk@hotmail.com>
2021-06-21 10:49:26 +0200

    AddedIntArray

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         int[] myIntArray = new int[5];
+        myIntArray[0] = 15;
+        myIntArray[1] = 5;
+        myIntArray[2] = 13;
+        myIntArray[3] = 99;
+        myIntArray[4] = 1;
+
 
 
     }
